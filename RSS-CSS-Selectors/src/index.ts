1a616ae8164211a0e60a1656b6fb10d523ccfb03
juliakocur <juliakocur@mail.com>
2023-06-16 10:46:37 +0200

    feat: add right block with a burger button

--- RSS-CSS-Selectors/src/index.ts
+++ RSS-CSS-Selectors/src/index.ts
@@ -1,2 +1,13 @@
 import './style.css';
+import './options.css';
 
+const burger = <HTMLElement>document.querySelector('.burger_wrap');
+const one = <HTMLElement>document.querySelector('.one');
+const two = <HTMLElement>document.querySelector('.two');
+const three = <HTMLElement>document.querySelector('.three');
+
+burger.addEventListener('click', function openMenu(): void {
+    one.classList.toggle('span_one');
+    two.classList.toggle('span_two');
+    three.classList.toggle('span_three');
+});
