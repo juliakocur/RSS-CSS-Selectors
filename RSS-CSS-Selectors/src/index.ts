41ec598c6c5b0f68122f9eabb795633c86466892
juliakocur <juliakocur@mail.com>
2023-06-19 11:35:50 +0200

    feat: add footer and burger-menu

--- RSS-CSS-Selectors/src/index.ts
+++ RSS-CSS-Selectors/src/index.ts
@@ -1,12 +1,21 @@
 import './style.css';
 import './options.css';
 
+const wrap = <HTMLElement>document.querySelector('.menu_wrap');
 const burger = <HTMLElement>document.querySelector('.burger_wrap');
 const one = <HTMLElement>document.querySelector('.one');
 const two = <HTMLElement>document.querySelector('.two');
 const three = <HTMLElement>document.querySelector('.three');
 
+wrap.addEventListener('click', function closeMenu(): void {
+    wrap.classList.remove('open');
+    one.classList.remove('span_one');
+    two.classList.remove('span_two');
+    three.classList.remove('span_three');
+});
+
 burger.addEventListener('click', function openMenu(): void {
+    wrap.classList.toggle('open');
     one.classList.toggle('span_one');
     two.classList.toggle('span_two');
     three.classList.toggle('span_three');
